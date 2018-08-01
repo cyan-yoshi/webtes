675d12e227060cc4c537e596d3cba7de555aa86a
root <root@192.168.42.105>
2018-08-01 18:53:22 +0900

    modify hello.go

--- go/hello.go
+++ go/hello.go
@@ -2,7 +2,7 @@
 package main
 
 import (
-//"fmt"
+"fmt"
 )
 
 func main() {
@@ -10,9 +10,9 @@ func main() {
         var vname1, vname2, vname3 int = 1, 2, 3
         //a:=1
         //fmt.Println(a)
-        //fmt.Println(vname1)
-        //fmt.Println(vname2)
-        //fmt.Println(vname3)
+        fmt.Println(vname1)
+        fmt.Println(vname2)
+        fmt.Println(vname3)
 
         fmt.Println("hello world")
 }
