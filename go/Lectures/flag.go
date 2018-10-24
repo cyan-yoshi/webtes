8920e23685735f38d49f27733cf609e1ee170823
cyan-yoshi <reina.yoshitani.5i@stu.hosei.ac.jp>
2018-10-24 15:25:45 +0900

    スライド p150 まで

---
+++ go/Lectures/flag.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"strings"
+)
+
+var msg = flag.String("msg", "デフォルト値", "使い方")
+var n int
+
+func init() {
+	flag.IntVar(&n, "n", 1, "回数")
+}
+
+func main() {
+	flag.Parse()
+	fmt.Println(strings.Repeat(*msg, n))
+}
